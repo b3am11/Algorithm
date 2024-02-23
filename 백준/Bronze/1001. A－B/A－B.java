0d24ddcafc229ed8a3556bab7f73a86a33225375
Yuri-Choi <101451715+b3am11@users.noreply.github.com>
2024-02-23 16:42:55 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
         System.out.println(a - b);
     }
 }
