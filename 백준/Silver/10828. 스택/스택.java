e824c80f8e2754e31a79d2d1c9b42d219331ce76
Yuri-Choi <101451715+b3am11@users.noreply.github.com>
2024-03-03 23:10:10 +0900

    Update 스택.java

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -5,7 +5,6 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
