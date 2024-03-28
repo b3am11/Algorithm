07c8a321ea9f69badf2ab01f24876a303ec89f09
Yuri <101451715+b3am11@users.noreply.github.com>
2024-03-28 14:12:38 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18104 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
-    
-    public static void main(String[] args) {
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        int arr[] = new int[x+1];
+        int[] dy = new int[N + 1];
+        dy[0] = dy[1] = 0;
+        for (int i = 2; i <= N; i++) {
+            dy[i] = dy[i - 1] + 1; //먼저 1을 빼준다.
+            if (i % 2 == 0) dy[i] = Math.min(dy[i], dy[i / 2] + 1);
+            if (i % 3 == 0) dy[i] = Math.min(dy[i], dy[i / 3] + 1);
 
-        arr[0] = arr[1] = 0;
-
-        for(int i = 2; i <= x; i++) {
-            arr[i] = arr[i-1] + 1;
-            if(i % 2 == 0) arr[i] = Math.min(arr[i], arr[i/2] + 1);
-            if(i % 3 == 0) arr[i] = Math.min(arr[i], arr[i/3] + 1);
         }
-
-        System.out.println(arr[x]);
-   
+        System.out.println(dy[N]);
     }
 }
