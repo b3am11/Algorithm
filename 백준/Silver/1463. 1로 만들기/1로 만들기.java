2d3762c637382e6a97580d66a5791044d59d66c3
Yuri <101451715+b3am11@users.noreply.github.com>
2024-03-28 14:04:02 +0900

    [Silver III] Title: 1로 만들기, Time: 232 ms, Memory: 21640 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int arr[] = new int[x+1];
+
+        arr[0] = arr[1] = 0;
+
+        for(int i = 2; i <= x; i++) {
+            arr[i] = arr[i-1] + 1;
+            if(i % 2 == 0) arr[i] = Math.min(arr[i], arr[i/2] + 1);
+            if(i % 3 == 0) arr[i] = Math.min(arr[i], arr[i/3] + 1);
+        }
+
+        System.out.println(arr[x]);
+   
+    }
+}
